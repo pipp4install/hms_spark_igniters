e7d426e401e17eaba5d131ad87922afb6f5a4164
Pippa H <70948349+pipp4install@users.noreply.github.com>
2022-11-09 11:36:13 +0000

    Load multiple files and concat into dataframe

--- pip_exploratory.py
+++ pip_exploratory.py
@@ -1,4 +1,12 @@
-from pyspark.sql import SparkSession, functions as F
+from functools import reduce
+
+from pyspark.sql import (
+    DataFrame,
+    functions as F,
+    SparkSession,
+
+)
+
 
 spark = (
     SparkSession.builder.appName("default-session")
@@ -17,12 +25,12 @@ def load_data(url):
     )
 
 
-def sum_price(df):
+def average_price(df):
     """Sum the price over each unique product and region."""
     return (
         df
-        .groupBy('quote_date', 'item_id', 'region').sum('price')
-        .withColumnRenamed('sum(price)','price')
+        .groupBy('quote_date', 'item_id', 'region').avg('price')
+        .withColumnRenamed('avg(price)','price')
         .withColumn('price', F.round(F.col('price'), scale=2))
     )
 
@@ -52,8 +60,56 @@ def decode_region(df):
         .dropna(subset=['region'])
     )
 
-url = "https://raw.githubusercontent.com/pipp4install/hms_spark_igniters/main/pricequotes202209.csv"
 
-df = load_data(url)
-df = sum_price(df)
-df = decode_region(df)
+def load_and_clean_data(urls):
+    """Load csv files, clean and concat into one dataframe."""
+    dfs = []
+
+    for url in urls:
+        df = load_data(url)
+        df = average_price(df)
+        df = decode_region(df)
+        dfs.append(df)
+
+    return reduce(DataFrame.unionByName, dfs)
+
+
+github = "https://raw.githubusercontent.com/"
+repo = "pipp4install/hms_spark_igniters/main/data/prices/"
+urls = [
+    f"{github}{repo}202001.csv",
+    f"{github}{repo}202002.csv",
+    f"{github}{repo}202003.csv",
+    f"{github}{repo}202004.csv",
+    f"{github}{repo}202005.csv",
+    f"{github}{repo}202006.csv",
+    f"{github}{repo}202007.csv",
+    f"{github}{repo}202008.csv",
+    f"{github}{repo}202009.csv",
+    f"{github}{repo}202010.csv",
+    f"{github}{repo}202011.csv",
+    f"{github}{repo}202012.csv",
+    f"{github}{repo}202101.csv",
+    f"{github}{repo}202102.csv",
+    f"{github}{repo}202103.csv",
+    f"{github}{repo}202104.csv",
+    f"{github}{repo}202105.csv",
+    f"{github}{repo}202106.csv",
+    f"{github}{repo}202107.csv",
+    f"{github}{repo}202108.csv",
+    f"{github}{repo}202109.csv",
+    f"{github}{repo}202110.csv",
+    f"{github}{repo}202111.csv",
+    f"{github}{repo}202112.csv",
+    f"{github}{repo}202201.csv",
+    f"{github}{repo}202202.csv",
+    f"{github}{repo}202203.csv",
+    f"{github}{repo}202204.csv",
+    f"{github}{repo}202205.csv",
+    f"{github}{repo}202206.csv",
+    f"{github}{repo}202207.csv",
+    f"{github}{repo}202208.csv",
+    f"{github}{repo}202209.csv",
+]
+
+df = load_and_clean_data(urls)
