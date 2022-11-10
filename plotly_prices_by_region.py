b915364ae0bb76f279889b0deb392aba2bc09c0a
dombean <46692370+dombean@users.noreply.github.com>
2022-11-10 15:55:03 +0000

    Removed plotly_prices_by_region.py Script

--- plotly_prices_by_region.py
+++
@@ -1,6 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_csv("./data/processed/prices_processed_by_category_region.csv", infer_datetime_format=True)
-# Beer Average Price Per Region
-px.line(df.query("desc == 'Beer'"), x="quote_date", y="price", color="region")
